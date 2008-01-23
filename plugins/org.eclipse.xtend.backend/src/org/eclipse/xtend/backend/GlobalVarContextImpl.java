2a07fa2829b5a889b28aae56a90bcb45212e8010
ahaase <ahaase>
2008-01-23 01:12:05 +0000

    reduced visibility of the class

--- plugins/org.eclipse.xtend.backend/src/org/eclipse/xtend/backend/GlobalVarContextImpl.java
+++ plugins/org.eclipse.xtend.backend/src/org/eclipse/xtend/backend/GlobalVarContextImpl.java
@@ -20,7 +20,7 @@ import org.eclipse.xtend.backend.common.GlobalVarContext;
  * 
  * @author Arno Haase (http://www.haase-consulting.com)
  */
-public final class GlobalVarContextImpl implements GlobalVarContext {
+final class GlobalVarContextImpl implements GlobalVarContext {
 	private final Map<String, Object> _globalVars = new HashMap<String, Object> ();
 
 	public Map<String, Object> getGlobalVars() {

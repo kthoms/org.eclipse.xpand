a88b99b80b30252293638f62bdcf295ad3136235
sefftinge <sefftinge>
2008-02-29 14:24:40 +0000

    static typesystem (first draft)

---
+++ plugins/org.eclipse.xpand3/src/org/eclipse/xand3/analyzation/typesystem/WildcardTypeArgument.java
@@ -0,0 +1,32 @@
+/**
+ * <copyright> 
+ *
+ * Copyright (c) 2002-2007 itemis AG and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   itemis AG - Initial API and implementation
+ *
+ * </copyright>
+ *
+ */
+package org.eclipse.xand3.analyzation.typesystem;
+
+/**
+ * @author Sven Efftinge
+ *
+ */
+public interface WildcardTypeArgument extends TypeArgument {
+	/**
+	 * @return the lower bound type (specified using e.g. <* super MyType>) 
+	 */
+	Type getLowerBound();
+	
+	/**
+	 * @return the upper bounds (specified using e.g. <* extends Comparable,Serializable>) 
+	 */
+	Type[] getUpperBounds();
+}

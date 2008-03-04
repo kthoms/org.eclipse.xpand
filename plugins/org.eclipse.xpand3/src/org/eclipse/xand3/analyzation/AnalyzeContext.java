d1ad1ec9f6e2d1f11dbc9b1d00afb766571defae
bkolb <bkolb>
2008-03-04 15:42:47 +0000

    sven will den haben

--- plugins/org.eclipse.xpand3/src/org/eclipse/xand3/analyzation/AnalyzeContext.java
+++ plugins/org.eclipse.xpand3/src/org/eclipse/xand3/analyzation/AnalyzeContext.java
@@ -15,10 +15,44 @@
  */
 package org.eclipse.xand3.analyzation;
 
+import java.util.Map;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.xpand3.Identifier;
+import org.eclipse.xpand3.declaration.AbstractDeclaration;
+import org.eclipse.xpand3.declaration.Extension;
+import org.eclipse.xpand3.expression.AbstractExpression;
+import org.eclipse.xpand3.staticTypesystem.AbstractTypeReference;
+import org.eclipse.xpand3.staticTypesystem.FunctionType;
+
 /**
  * @author Sven Efftinge
- *
+ * @author Bernd Kolb
  */
 public interface AnalyzeContext {
-	
+
+	String IMPLICIT_VARIABLE = null;
+
+	Map<String, ? extends AbstractTypeReference> getLocalVars();
+
+	boolean hasThis();
+
+	Object getThis();
+
+	AnalyzeContext cloneWithVariable(Identifier eleName,
+			AbstractTypeReference object);
+
+	AbstractTypeReference getVariable(String varName);
+
+	AbstractExpression getExtensionForTypes(String functionName,
+			AbstractTypeReference[] paramTypes);
+
+	FunctionType getOperationFor(AbstractTypeReference target,
+			String functionName, AbstractTypeReference[] withoutFirst);
+
+	AbstractExpression getTypeForName(Identifier typeLiteral);
+
+	Extension findExtension(String expression,
+			EList<AbstractDeclaration> declarations);
+
 }

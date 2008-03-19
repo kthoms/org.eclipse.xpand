10a058dca1e6a1c6e0f725c696b0cf71da45c9ba
ahaase <ahaase>
2008-03-19 20:08:26 +0000

    reverted to good layout

--- plugins/org.eclipse.xtend.backend/src/org/eclipse/xtend/backend/functions/SourceDefinedFunction.java
+++ plugins/org.eclipse.xtend.backend/src/org/eclipse/xtend/backend/functions/SourceDefinedFunction.java
@@ -18,53 +18,53 @@ import org.eclipse.xtend.backend.common.ExpressionBase;
 import org.eclipse.xtend.backend.common.LocalVarContext;
 import org.eclipse.xtend.backend.common.StacktraceEntry;
 
+
 /**
  * 
  * @author Arno Haase (http://www.haase-consulting.com)
  */
 public final class SourceDefinedFunction extends AbstractFunction {
-	private final String _name;
-	private final List<String> _paramNames;
-	private final ExpressionBase _def;
+    private final String _name;
+    private final List<String> _paramNames;
+    private final ExpressionBase _def;
 
-	public SourceDefinedFunction(String name, List<String> paramNames,
-			List<BackendType> paramTypes, ExpressionBase def, boolean cached,
-			ExpressionBase guard) {
-		super(guard, paramTypes, cached);
-		_name = name;
-		_paramNames = paramNames;
-		_def = def;
-	}
+    public SourceDefinedFunction (String name, List<String> paramNames, List<BackendType> paramTypes, ExpressionBase def, boolean cached, ExpressionBase guard) {
+        super (guard, paramTypes, cached);
+        _name = name;
+        _paramNames = paramNames;
+        _def = def;
+    }
 
-	public Object invoke(ExecutionContext ctx, Object[] params) {
-		final LocalVarContext lvc = new LocalVarContext();
-		for (int i = 0; i < _paramNames.size(); i++) {
-			lvc.getLocalVars().put(_paramNames.get(i), params[i]);
-		}
+    public Object invoke (ExecutionContext ctx, Object[] params) {
+        final LocalVarContext lvc = new LocalVarContext ();
+        for (int i=0; i<_paramNames.size(); i++) {
+            lvc.getLocalVars().put(_paramNames.get(i), params[i]);
+        }
+        
+        final LocalVarContext oldLvc = ctx.getLocalVarContext();
+        try {
+            ctx.setLocalVarContext(lvc);
+            
+            if (ctx.isLogStacktrace())
+                ctx.getStacktrace().add (new StacktraceEntry (_def.getPos(), ctx));
+            
+            return _def.evaluate(ctx);
+        }
+        finally {
+            if (ctx.isLogStacktrace())
+                ctx.getStacktrace().remove (ctx.getStacktrace().size() - 1);
+            
+            ctx.setLocalVarContext(oldLvc);
+        }
+    }
 
-		final LocalVarContext oldLvc = ctx.getLocalVarContext();
-		try {
-			ctx.setLocalVarContext(lvc);
-
-			if (ctx.isLogStacktrace())
-				ctx.getStacktrace()
-						.add(new StacktraceEntry(_def.getPos(), ctx));
-
-			return _def.evaluate(ctx);
-		} finally {
-			if (ctx.isLogStacktrace())
-				ctx.getStacktrace().remove(ctx.getStacktrace().size() - 1);
-
-			ctx.setLocalVarContext(oldLvc);
-		}
-	}
-
-	@Override
-	public String toString() {
-		return "SourceDefinedFunction '" + _name + "' " + getParameterTypes();
-	}
-
-	public String getName() {
-		return _name;
-	}
+    public String getName () {
+        return _name;
+    }
+    
+    @Override
+    public String toString () {
+        return "SourceDefinedFunction '" + _name + "' " + getParameterTypes();
+    }
 }
+

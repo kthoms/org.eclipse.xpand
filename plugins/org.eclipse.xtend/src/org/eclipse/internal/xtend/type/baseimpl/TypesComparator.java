b209dddd1c4f3fd1c0aa7c5de664cdc5aa4e6253
dhubner <dhubner>
2009-10-23 10:12:38 +0000

    little performance optimization

--- plugins/org.eclipse.xtend/src/org/eclipse/internal/xtend/type/baseimpl/TypesComparator.java
+++ plugins/org.eclipse.xtend/src/org/eclipse/internal/xtend/type/baseimpl/TypesComparator.java
@@ -17,33 +17,35 @@ import java.util.List;
 import org.eclipse.xtend.typesystem.Type;
 
 public final class TypesComparator implements Comparator<List<? extends Type>> {
-    /**
-     * 
-     * returns -1 if the second list of types is not assignable to the first
-     * list of types returns 0 if the second list of types exactly matches the
-     * first list of types returns 1 if the second list of types is assignable
-     * to the first list of types
-     */
-    public int compare(final List<? extends Type> types1, final List<? extends Type> types2) {
-        if (types1 == null || types2 == null)
-            throw new NullPointerException();
-        if (types1.size() != types2.size())
-            return -1;
-        boolean directMatch = true;
-        for (int i = 0, x = types1.size(); i < x; i++) {
-            final Type type1 = types1.get(i);
-            final Type type2 = types2.get(i);
-            if (type1.isAssignableFrom(type2)) {
-                if (!type1.equals(type2)) {
-                    directMatch = false;
-                }
-            } else
-                return -1;
-        }
-        if (directMatch)
-            return 0;
-        else
-            return 1;
-    }
+	/**
+	 * 
+	 * returns -1 if the second list of types is not assignable to the first
+	 * list of types returns 0 if the second list of types exactly matches the
+	 * first list of types returns 1 if the second list of types is assignable
+	 * to the first list of types
+	 */
+	public int compare(final List<? extends Type> types1,
+			final List<? extends Type> types2) {
+		if (types1 == null || types2 == null)
+			throw new NullPointerException();
+		if (types1.size() != types2.size())
+			return -1;
+		boolean directMatch = true;
+		for (int i = 0, x = types1.size(); i < x; i++) {
+			final Type type1 = types1.get(i);
+			final Type type2 = types2.get(i);
+			if (type1.isAssignableFrom(type2)) {
+				if (directMatch && !type1.equals(type2)) {
+					directMatch = false;
+				}
+			} else {
+				return -1;
+			}
+		}
+		if (directMatch)
+			return 0;
+		else
+			return 1;
+	}
 
 }

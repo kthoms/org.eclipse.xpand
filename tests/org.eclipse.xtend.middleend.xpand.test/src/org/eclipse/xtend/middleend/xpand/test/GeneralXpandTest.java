dc1e015ad1f0aa1946d1483fda0c92c81725e1be
kthoms <kthoms>
2010-01-05 07:31:25 +0000

    RESOLVED - bug 298794: Move XpandTokens to org.eclipse.internal.xpand2 https://bugs.eclipse.org/bugs/show_bug.cgi?id=298794

--- tests/org.eclipse.xtend.middleend.xpand.test/src/org/eclipse/xtend/middleend/xpand/test/GeneralXpandTest.java
+++ tests/org.eclipse.xtend.middleend.xpand.test/src/org/eclipse/xtend/middleend/xpand/test/GeneralXpandTest.java
@@ -31,7 +31,7 @@ import org.eclipse.emf.mwe.core.WorkflowInterruptedException;
 import org.eclipse.emf.mwe.core.issues.Issues;
 import org.eclipse.emf.mwe.core.issues.IssuesImpl;
 import org.eclipse.emf.mwe.core.monitor.NullProgressMonitor;
-import org.eclipse.internal.xpand2.codeassist.XpandTokens;
+import org.eclipse.internal.xpand2.XpandTokens;
 import org.eclipse.xpand2.output.Outlet;
 import org.eclipse.xtend.backend.BackendFacade;
 import org.eclipse.xtend.backend.common.ExecutionContext;

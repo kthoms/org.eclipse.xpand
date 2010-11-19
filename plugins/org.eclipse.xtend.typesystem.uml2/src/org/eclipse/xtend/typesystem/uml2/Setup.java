d429bb9fe0e646cd7f4552cf487bc3d3d9c79f3d
Karsten Thoms <karsten.thoms@itemis.de>
2010-11-19 18:03:35 +0100

    Fix Bug#324902
    Registering UMLPackage for UML210 NS URI

--- plugins/org.eclipse.xtend.typesystem.uml2/src/org/eclipse/xtend/typesystem/uml2/Setup.java
+++ plugins/org.eclipse.xtend.typesystem.uml2/src/org/eclipse/xtend/typesystem/uml2/Setup.java
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.eclipse.xtend.typesystem.uml2;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.mapping.ecore2xml.Ecore2XMLPackage;
@@ -21,70 +23,74 @@ import org.eclipse.xtend.typesystem.emf.Mapping;
  * Initialization of UML2 Support. This class must be integrated into workflows
  * using EMF UML2 like follows:
  * <p>
- * 
+ *
  * <pre>
  *  	&lt;bean class=&quot;org.eclipse.internal.xtend.uml2.Setup&quot;&gt;
  *  		&lt;standardUML2Setup value=&quot;true&quot;/&gt;
  * 	&lt;/bean&gt;
  * </pre>
- * 
+ *
  * @author Sven Efftinge
  * @author Karsten Thoms (docs)
  */
 public class Setup extends org.eclipse.xtend.typesystem.emf.Setup {
-	// 
+	private static Log LOG = LogFactory.getLog(Setup.class);
+	//
 	private static final String UML2_200_NS_URI = "http://www.eclipse.org/uml2/2.0.0/UML";
+	private static final String UML2_210_NS_URI = "http://www.eclipse.org/uml2/2.1.0/UML";
 	/**
 	 * Constructor. Registers the UMLPackage.
-	 * 
+	 *
 	 */
 	public Setup() {
 		addEPackageClass(UMLPackage.eINSTANCE.getClass().getName());
+		doSetup();
 	}
 
 	/**
-	 * Registers all required UML resources.
-	 * 
-	 * @param b
-	 *            Initialization occurs only if <code>true</code> is passed.
+	 * @deprecated Not required anymore, since setup is already done on instantiation
 	 */
+	@Deprecated
 	public void setStandardUML2Setup(boolean b) {
-		if (b) {
-			addExtensionMap(new Mapping("ecore", "org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl"));
-			addExtensionMap(new Mapping("uml", "org.eclipse.uml2.uml.resource.UMLResource"));
-			addExtensionMap(new Mapping("uml2", "org.eclipse.uml2.uml.resource.UML22UMLResource"));
-			String p = "metamodels/UML.metamodel.uml";
-			URI uri = EcoreUtil2.getURI(p);
-			if (uri != null && !uri.toString().equals(p)) {
-				String path = uri.toString();
-				final int mmIndex = path.lastIndexOf("/metamodels");
-				if (mmIndex < 0)
-					throw new IllegalStateException("Missing required plugin 'org.eclipse.uml2.uml.resources' in classpath.");
-					
-				path = path.substring(0, mmIndex);
-				path = addJarProtocolIfNecessary(path);
-				addUriMap(new Mapping("pathmap://UML_PROFILES/", path + "/profiles/"));
-				addUriMap(new Mapping("pathmap://UML_METAMODELS/", path + "/metamodels/"));
-				addUriMap(new Mapping("pathmap://UML_LIBRARIES/", path + "/libraries/"));
-			} else {
+		LOG.warn("The property 'standardUML2Setup' is obsolete. Remove it from your workflow configuration.");
+	}
+
+	protected void doSetup() {
+		addExtensionMap(new Mapping("ecore", "org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl"));
+		addExtensionMap(new Mapping("uml", "org.eclipse.uml2.uml.resource.UMLResource"));
+		addExtensionMap(new Mapping("uml2", "org.eclipse.uml2.uml.resource.UML22UMLResource"));
+		String p = "metamodels/UML.metamodel.uml";
+		URI uri = EcoreUtil2.getURI(p);
+		if (uri != null && !uri.toString().equals(p)) {
+			String path = uri.toString();
+			final int mmIndex = path.lastIndexOf("/metamodels");
+			if (mmIndex < 0)
 				throw new IllegalStateException("Missing required plugin 'org.eclipse.uml2.uml.resources' in classpath.");
-			}
-			// Register old UML2 URIs which is not done in Java Application mode. Running Eclipse this
-			// part is done automatically using the eclipse extension registry and EMF metamodel Extension
-			// which is generated for all EPackages
-			EPackage.Registry.INSTANCE.put(UML2_200_NS_URI, EPackage.Registry.INSTANCE.get(UMLPackage.eINSTANCE.getNsURI()));
-			Ecore2XMLPackage.eINSTANCE.getEClassifiers();
-			uri = EcoreUtil2.getURI("model/UML2_2_UML.ecore2xml");
-			if (uri != null) {
-				String path = addJarProtocolIfNecessary(uri.toString());
-				addUriMap(new Mapping("platform:/plugin/org.eclipse.uml2.uml/model/UML2_2_UML.ecore2xml", path));
-			}
+
+			path = path.substring(0, mmIndex);
+			path = addJarProtocolIfNecessary(path);
+			addUriMap(new Mapping("pathmap://UML_PROFILES/", path + "/profiles/"));
+			addUriMap(new Mapping("pathmap://UML_METAMODELS/", path + "/metamodels/"));
+			addUriMap(new Mapping("pathmap://UML_LIBRARIES/", path + "/libraries/"));
+		} else {
+			throw new IllegalStateException("Missing required plugin 'org.eclipse.uml2.uml.resources' in classpath.");
+		}
+		// Register old UML2 URIs which is not done in Java Application mode. Running Eclipse this
+		// part is done automatically using the eclipse extension registry and EMF metamodel Extension
+		// which is generated for all EPackages
+		EPackage.Registry.INSTANCE.put(UML2_200_NS_URI, EPackage.Registry.INSTANCE.get(UMLPackage.eINSTANCE.getNsURI()));
+		EPackage.Registry.INSTANCE.put(UML2_210_NS_URI, EPackage.Registry.INSTANCE.get(UMLPackage.eINSTANCE.getNsURI()));
+		Ecore2XMLPackage.eINSTANCE.getEClassifiers();
+		uri = EcoreUtil2.getURI("model/UML2_2_UML.ecore2xml");
+		if (uri != null) {
+			String path = addJarProtocolIfNecessary(uri.toString());
+			addUriMap(new Mapping("platform:/plugin/org.eclipse.uml2.uml/model/UML2_2_UML.ecore2xml", path));
 		}
 	}
 
 	/**
 	 * Fixes a resource path of a Jar file, if necessary.
-	 * 
+	 *
 	 * @param path
 	 *            A resource path
 	 * @return If the path ends with '.jar', but has no protocol prefix 'jar:',

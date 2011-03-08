9d5302bf8a303b7ad7189fa96a103a7cb50c80f2
Karsten Thoms <karsten.thoms@itemis.de>
2011-03-08 10:03:03 +0100

    Applied patch attachment#190628 for bug#339169 Make use of MarkerFactory

--- plugins/org.eclipse.xtend.shared.ui/src/org/eclipse/xtend/shared/ui/core/builder/XtendXpandMarkerManager.java
+++ plugins/org.eclipse.xtend.shared.ui/src/org/eclipse/xtend/shared/ui/core/builder/XtendXpandMarkerManager.java
@@ -25,9 +25,14 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.ui.actions.WorkspaceModifyOperation;
@@ -38,6 +43,10 @@ import org.eclipse.xtend.shared.ui.internal.XtendLog;
 
 public class XtendXpandMarkerManager {
 
+	private static final String MARKER_FACTORY_ID = "org.eclipse.xtend.shared.ui.markerFactory";
+	
+	private static IMarkerFactory markerFactory;
+	
 	private static Map<IFile, Set<MarkerObject>> markers = new HashMap<IFile, Set<MarkerObject>>();
 	
     public static final String getMARKER_TYPE() {
@@ -51,23 +60,28 @@ public class XtendXpandMarkerManager {
                 start = issue.getElement().getStart();
                 end = issue.getElement().getEnd();
             }
-            internalAddMarker(file, issue.getMessage(), severity, start, end);
+            internalAddMarker(file, issue.getMessage(), severity, start, end, issue.getType().toString());
     }
 
     public static void addErrorMarker(final IFile file, final String message, final int severity, final int start,
             final int end) {
-            internalAddMarker(file, message, severity, start, end);
+            internalAddMarker(file, message, severity, start, end, null);
     }
 
+    public static void addErrorMarker(final IFile file, final String message, final int severity, final int start,
+            final int end, String issueType) {
+            internalAddMarker(file, message, severity, start, end, issueType);
+    }
+    
     public static void addWarningMarker(final IFile file, final String message, final int severity, final int start,
             final int end) {
-            internalAddMarker(file, message, severity, start, end);
+            internalAddMarker(file, message, severity, start, end, null);
     }
 
     
     private final static void internalAddMarker(final IFile file, final String message,
-            final int severity, final int start, final int end) {
-    	MarkerObject mo = new MarkerObject(message, severity, start, end);
+            final int severity, final int start, final int end, String issueType) {
+    	MarkerObject mo = new MarkerObject(message, severity, start, end, issueType);
     	if (markers.get(file)==null){
     			Set<MarkerObject> mos = new HashSet<MarkerObject>();
     			mos.add(mo);
@@ -93,52 +107,41 @@ public class XtendXpandMarkerManager {
 				protected void execute(final IProgressMonitor monitor) throws CoreException, InvocationTargetException,
 						InterruptedException {
 
-					try {
 						iFile.deleteMarkers(getMARKER_TYPE(), true, IResource.DEPTH_INFINITE);
-						for (MarkerObject markerObject : mos) {
-							IMarker marker = iFile.createMarker(getMARKER_TYPE());
-							marker.setAttribute(IMarker.MESSAGE, markerObject.getMessage());
-							marker.setAttribute(IMarker.SEVERITY, markerObject.getSeverity());
-							int s = markerObject.getStart();
-							if (markerObject.getStart() == -1) {
-								s = 1;
-							}
-							int e = markerObject.getEnd();
-							if (markerObject.getEnd() <= markerObject.getStart()) {
-								e = markerObject.getStart() + 1;
-							}
-							marker.setAttribute(IMarker.CHAR_START, s);
-							marker.setAttribute(IMarker.CHAR_END, e);
-							final ITextFileBufferManager mgr = FileBuffers.getTextFileBufferManager();
-							if (mgr != null) {
-								final IPath location = iFile.getFullPath();
-								try {
-									mgr.connect(location, LocationKind.NORMALIZE, new NullProgressMonitor());
-									final ITextFileBuffer buff = mgr.getTextFileBuffer(iFile.getFullPath(),
-											LocationKind.NORMALIZE);
-									if (buff != null) {
-										final IDocument doc = buff.getDocument();
-										final int line = doc.getLineOfOffset(markerObject.getStart());
-										if (line > 0) {
-											marker.setAttribute(IMarker.LINE_NUMBER, doc.getLineOfOffset(markerObject.getStart()));
-											marker.setAttribute(IMarker.LOCATION, Messages.XtendXpandMarkerManager_Line
-													+ line);
+						final ITextFileBufferManager mgr = FileBuffers.getTextFileBufferManager();
+						final IPath location = iFile.getFullPath();
+						if (mgr != null) {
+							try {
+								mgr.connect(location, LocationKind.NORMALIZE, new NullProgressMonitor());
+								final ITextFileBuffer buff = mgr.getTextFileBuffer(iFile.getFullPath(),
+										LocationKind.NORMALIZE);
+								if (buff != null) {
+									final IDocument doc = buff.getDocument();
+									for (MarkerObject markerObject : mos) {
+										int line =0;
+										try {
+											line = doc.getLineOfOffset(markerObject.getStart());
 										}
+										catch (BadLocationException e) {
+											XtendLog.logError(e);
+										}
+										//call markerFactory and create the marker
+										getMarkerFactory().createMarker(iFile, markerObject.getMessage(),
+												markerObject.getSeverity(), markerObject.getStart(),
+												markerObject.getEnd(), line,
+												Messages.XtendXpandMarkerManager_Line + line, markerObject.getIssueType());
 									}
 								}
-								finally {
-									mgr.disconnect(location, LocationKind.NORMALIZE, new NullProgressMonitor());
-								}
+							}catch (final CoreException e) {
+								XtendLog.logError(e);
+							}
+							finally {
+								mgr.disconnect(location, LocationKind.NORMALIZE, new NullProgressMonitor());
 							}
 						}
-					}
-					catch (final CoreException e) {
-						XtendLog.logError(e);
-					}
-					catch (final BadLocationException e) {
-						XtendLog.logError(e);
-					}
+					
 				}
+
 			}.run(new NullProgressMonitor());
 		}
 		catch (final Exception e) {
@@ -163,4 +166,42 @@ public class XtendXpandMarkerManager {
             XtendLog.logError(ce);
         }
     }
+
+	/**
+	 * getting MarkerFactory from ExtensionPoint
+	 */
+	private static synchronized IMarkerFactory getMarkerFactory() {
+		if (markerFactory == null){
+			IExtensionRegistry extRegistry = Platform.getExtensionRegistry();
+			IExtensionPoint extPoint = extRegistry.getExtensionPoint(MARKER_FACTORY_ID);
+			if (extPoint != null){									  
+				IExtension[] extensions = extPoint.getExtensions();
+				if (extensions.length>=1){
+					IExtension extension = extensions[0];
+					IConfigurationElement[] configElements = extension.getConfigurationElements();
+					if (configElements.length>=1){
+						try {
+							markerFactory = (IMarkerFactory)configElements[0].createExecutableExtension("class");
+						}
+						catch (CoreException e) {
+							markerFactory = new IMarkerFactory.Default();
+							XtendLog.logError("Problem with extension \""+configElements[0].getName()+"\" using default implementation instead", e);
+						}
+					} if (configElements.length > 1){
+						XtendLog.logInfo("more than one MarkerFactory defined. Taking\""+configElements[0].getName()+"\"");
+					}
+				}if (extensions.length>1){
+					XtendLog.logInfo("more than one MarkerFactory defined. Taking\""+extensions[0].getExtensionPointUniqueIdentifier()+"\"");
+				}if (extensions.length==0){
+					markerFactory=new IMarkerFactory.Default();
+					XtendLog.logInfo("No MarkerFactory defined. Taking default implementation");
+				}
+			}else {
+				markerFactory=new IMarkerFactory.Default();
+				XtendLog.logInfo("No MarkerFactory defined. Taking default implementation");
+			}
+		}
+		return markerFactory;
+	}
+	
 }
